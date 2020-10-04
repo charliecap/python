769fa8eb3107c953c1b93da1fd250ff34094ea60
charliecap <charlesclinesdesk@gmail.com>
2020-10-04 18:17:08 -0400

    .

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+# from datetime import date
+# import datetime
+
+
+
+# def calculateAge(born):
+#     today = date.today()
+#     try:
+#         birthday = born.replace(year=today.year)
+
+#     # raised when birth date is February 29
+#     # and the current year is not a leap year
+#     except ValueError:
+#         birthday = born.replace(year=today.year,
+#                                 month=born.month + 1, day=1)
+
+#     if birthday > today:
+#         return today.year - born.year - 1
+#     else:
+#         return today.year - born.year
+
+
+# my_name = "Charlie"
+# my_age = calculateAge(date(1988, 6, 8))
+# my_trainer_id = "CharlieCap"
+
+# print(my_name, my_age, my_trainer_id)
+
+
